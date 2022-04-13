0fcfe082c346559c64216340bec30754db2cb3f8
Christopher Pham <cpham5@umbc.edu>
2022-04-12 23:37:25 -0400

    implemented login page, stories now store with username

--- tmasTest/views.py
+++ tmasTest/views.py
@@ -17,7 +17,7 @@ def add(request):
     date = request.POST['date']
     story = request.POST['story']
     storyID = request.POST['storyID']
-    user = request.POST['user']
+    user = request.user.get_username()
     location = request.POST['location']
     community = request.POST['community']
     story = Tmas(subject=subject, date=date, story=story, storyID=storyID, user=user, location=location,
